278f4efb34d61fadf8f367c8dc55847cbe8a639b
Street Coder Nate B <natebaker2427@gmail.com>
2023-07-31 17:35:30 -0400

    upload script update and prompts updated.

--- upload_bots_to_db.py
+++ upload_bots_to_db.py
@@ -13,7 +13,7 @@ import json, sys
 
 def upload_bots_from_list(bot):
     db = client["Bots"]
-    collection = db["Social Media Bots"]
+    collection = db["Social Media Bots 2"]
     return collection.insert_many(bot)
 
 # Loads the config
@@ -87,5 +87,32 @@ def upload_accs_accts_to_db(file_path):
     # save the accounts to the database
     upload_bots_from_list(accounts)
 
+def upload_bots_from_csv(file_path):
+    # open the text file
+    accounts = []
+    with open(file_path) as file:
+        for line in file:
+            # split the line into a list
+            line = line.split(",")
+            # get the username which is the first item in the line
+            username = line[0]
+            # get the password which is the second item in the line
+            password = line[1]
+            # get the email which is the third item in the line
+            email = line[2]
+            # get the email password which is the fourth item in the line
+            email_password = line[3] # SOMETIMES THIS LINE CHANGES
+            # create bot object
+            bot = {
+                "insta_name": username,
+                "insta_pwd": password,
+                "email_name": email,
+                "email_pwd": email_password,
+                "insta_in_use": False
+            }
+            # add to accounts as an list
+            accounts.append(bot)
+    # save the accounts to the database
+    upload_bots_from_list(accounts)
 
-upload_accs_accts_to_db("./bot_accounts/insta_accts_0.txt")
+upload_bots_from_csv("./bot_accounts/Nate - Project - Sheet2.csv")
